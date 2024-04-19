c8366962816eb7f8d87ae1a159c62a7db3d6a7a0
Ambitious905837 <tj905837@outlook.com>
2024-04-19 14:49:28 +0800

    Updated MoreSteps.jsx

--- src/components/signup/MoreSteps.tsx
+++ src/components/signup/MoreSteps.tsx
@@ -60,11 +60,9 @@ const MoreSteps: React.FC<MoreStepsProps> = ({
         try {
           const userCredential = await createUserWithEmailAndPassword(auth, collectedData.email, collectedData.password);
           await sendEmailVerification(userCredential.user);
+          console.log(userCredential.user, "---------------")
           // Additional code...
-        } catch (error: any) {
-          console.error("Failed to create user and send verification email", error);
-          toast.error("Registration failed: " + error.message);
-        }
+
         fetch(`${process.env.REACT_APP_API_URL}/users/signup`, {
           method: "POST",
           headers: {
@@ -98,6 +96,10 @@ const MoreSteps: React.FC<MoreStepsProps> = ({
           .finally(() => {
             setLoading(false);
           });
+        } catch (error: any) {
+          console.error("Failed to create user and send verification email", error);
+          toast.error("Registration failed: " + error.message);
+        }
       },
     });
 
